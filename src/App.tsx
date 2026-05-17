f8cfe7efe3ac4c4f17e8ccab25f2be2be850599b
uniquesonu <21bcs10550@cuchd.in>
2026-05-17 14:32:00 +0530

    add existing files

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { RtkMeeting } from '@cloudflare/realtimekit-react-ui';
+import { useRealtimeKitClient } from '@cloudflare/realtimekit-react';
+
+function App() {
+  const [meeting, initMeeting] = useRealtimeKitClient();
+
+  useEffect(() => {
+    const searchParams = new URL(window.location.href).searchParams;
+
+    const authToken = searchParams.get('authToken');
+
+    if (!authToken) {
+      alert(
+        "An authToken wasn't passed, please pass an authToken in the URL query to join a meeting."
+      );
+      return;
+    }
+
+    initMeeting({
+      authToken,
+      baseURI: import.meta.env.VITE_BASE_URL
+    });
+  }, []);
+
+  // By default this component will cover the entire viewport.
+  // To avoid that and to make it fill a parent container, pass the prop:
+  // `mode="fill"` to the component.
+  return <RtkMeeting meeting={meeting!} />;
+}
+
+export default App;
